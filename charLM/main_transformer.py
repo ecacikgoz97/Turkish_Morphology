1c6e60c4be274eadf1ad35734582b1246bb20a58
ecacikgoz97 <ecacikgoz97@gmail.com>
2021-12-21 02:18:06 +0300

    main updated

--- charLM/main_transformer.py
+++ charLM/main_transformer.py
@@ -1,11 +1,11 @@
 import argparse, torch, json, matplotlib, os
 import matplotlib.pyplot as plt
 from models.gpt3 import GPT3
+from train import train
 from common.utils import *
 from data import build_data
 matplotlib.use('Agg')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-from train import train
 
 
 # CONFIG
@@ -16,7 +16,7 @@ args.device = 'cuda'
 # training
 args.batchsize = 128
 args.epochs = 35
-args.opt= 'Adam'
+args.opt= 'WAdam'
 args.lr = 0.001
 args.task = 'lm'
 args.seq_to_no_pad = 'surface'
@@ -27,29 +27,30 @@ args.valdata = '/home/emrecan/Desktop/NLP/Turkish_Morphology/charLM/data/surf.un
 args.tstdata = args.valdata
 args.surface_vocab_file = args.trndata
 #args.maxtrnsize = 57769; args.maxvalsize = 10000; args.maxtstsize = 10000
-args.maxtrnsize = 100; args.maxvalsize = 50; args.maxtstsize = 50
+args.maxtrnsize = 5700; args.maxvalsize = 500; args.maxtstsize = 500
 rawdata, batches, vocab = build_data(args)
 trndata, vlddata, tstdata = rawdata
 args.trnsize , args.valsize, args.tstsize = len(trndata), len(vlddata), len(trndata)
 
 # model
-args.mname = 'charlm'
+args.mname = 'charlm_transformer'
 model_init = uniform_initializer(0.01)
 emb_init = uniform_initializer(0.1)
-embed_dim= 512; args.nh = 350
-args.enc_dropout_in = 0.2; args.enc_dropout_out = 0.3
+num_layers=5; embed_dim=64; num_heads=16; block_size=128
+embedding_dropout_rate=0.1; attention_dropout_rate=0.1; residual_dropout_rate=0.1
 args.model = GPT3(vocab=vocab,
-                  num_layers=8,
+                  num_layers=num_layers,
                   embed_dim=embed_dim,
-                  num_heads=8,
-                  block_size=128,
-                  embedding_dropout_rate=0.1, attention_dropout_rate=0.1, residual_dropout_rate=0.1,
+                  num_heads=num_heads,
+                  block_size=block_size,
+                  embedding_dropout_rate=embedding_dropout_rate,
+                  attention_dropout_rate=attention_dropout_rate,
+                  residual_dropout_rate=residual_dropout_rate,
                   expand_ratio=4)
-#print(args.model)
 args.model.to(args.device)
 
 # logging
-args.modelname = 'logging_transformer/'+args.mname+'/results/'+str(len(trndata))+'_instances/'
+args.modelname = 'logging_transformer/'+args.mname+'/results/'+str(len(trndata))+'_instances/'+str(num_layers)+'_'+str(embed_dim)+'_'+str(num_heads)
 try:
     os.makedirs(args.modelname)
     print("Directory " , args.modelname ,  " Created ")
