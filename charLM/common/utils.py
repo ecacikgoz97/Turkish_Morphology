ab953e7795d88247755980cae532646db3ed50a9
ecacikgoz97 <ecacikgoz97@gmail.com>
2022-01-02 03:05:35 +0300

    convert time function added

--- charLM/common/utils.py
+++ charLM/common/utils.py
@@ -77,3 +77,12 @@ def find_li_vectors(dim, R):
         i += 1
 
     return R_independent
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
